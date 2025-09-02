698b09078859f3e05ae9957932fcdaf1d39cffb3
Prashant Kumar Gupta <prashantkumargupta@Prashants-MacBook-Air.local>
2025-09-02 22:33:07 +0530

    thread safe singleton pattern

--- src/design_patterns/singleton_pattern/Singleton.java
+++ src/design_patterns/singleton_pattern/Singleton.java
@@ -1,5 +1,16 @@
 package design_patterns.singleton_pattern;
 
-public class Singleton {
+public final class Singleton { // final will ensure no one can extend this class
+    private static Singleton instance;
 
+    private Singleton(){
+
+    }
+
+    public static synchronized Singleton getInstance() {
+        if (instance == null) {
+            instance = new Singleton();
+        }
+        return instance;
+    }
 }
